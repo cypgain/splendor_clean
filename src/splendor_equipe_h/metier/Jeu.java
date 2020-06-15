1bb037860c1ea8403c2dfccd0ba4b5cf42af6f2d
GingembreVert <jbcavelier@yahoo.fr>
2020-06-15 09:48:05 +0200

    fix

--- src/splendor_equipe_h/metier/Jeu.java
+++ src/splendor_equipe_h/metier/Jeu.java
@@ -136,6 +136,7 @@ public class Jeu implements Serializable
             if (this.tabJetonsChoisis[i]==indexJeton)
             {
                 this.tabJetonsChoisis[i] = -1;
+                return;
             }
         }
     }
